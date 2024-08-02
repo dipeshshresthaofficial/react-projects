a05820ceff256eb2a83213e8e4fac5f28f288eb4
dipesh <dipeshshrestha0314@gmail.com>
2024-08-02 10:41:06 -0400

    added Slider component

--- lucid_bmi_calculator/src/App.jsx
+++ lucid_bmi_calculator/src/App.jsx
@@ -1,11 +1,36 @@
 import { useState } from 'react'
 import './App.css'
+import Slider from './components/Slider'
 
 function App() {
 
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  function handleSliderChange(value, title){
+    console.log(value +" " + title);
+    if(title == "Weight") setWeight(value);
+    else if (title == "Height") setHeight(value);
+  }
+
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>BMI Calculator</h1>
+      <div>
+        <Slider 
+          sliderTitle='Weight' 
+          sliderValue='40'
+          sliderSuffix='kg'
+          handleSliderChange = {handleSliderChange}
+          />
+        <Slider 
+          sliderTitle='Height' 
+          sliderValue='40'
+          sliderSuffix='cm'
+          max='250'
+          handleSliderChange = {handleSliderChange}
+          />
+      </div>
     </>
   )
 }
---
+++ lucid_bmi_calculator/src/components/Slider.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Slider({sliderTitle, sliderValue = '40', sliderSuffix, min='40', max='200', step='1', handleSliderChange}) {
+  return (
+    <div className='my-3 flex flex-col gap-2'>
+        <label className='text-start'>{sliderTitle+": "+sliderValue + sliderSuffix}</label>
+        <input 
+            type="range"
+            defaultValue={sliderValue}
+            min={min}
+            max={max}
+            step={step}
+            onChange={ e => handleSliderChange(e.target.value, sliderTitle)}
+            />
+    </div>
+  )
+}
+
+export default Slider
