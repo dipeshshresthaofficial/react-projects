165c8c55a15de01537098f923d8747895cb150ba
dipesh <dipeshshrestha0314@gmail.com>
2024-08-04 13:59:07 -0400

    updated layout, added array of transactions

--- lucid_expense_tracker/src/App.jsx
+++ lucid_expense_tracker/src/App.jsx
@@ -8,6 +8,26 @@ function App() {
 
   const [transactionTitle, setTransactionTitle] = useState('')
   const [transactionAmount, setTransactionAmount] = useState(0)
+  const [transactions, setTransactions] = useState([
+    {
+      "id": 1,
+      "title": "Shopping",
+      "amount": -150,
+      "type": "expense"
+    },
+    {
+      "id": 2,
+      "title": "Food",
+      "amount": -70,
+      "type": "expense"
+    },
+    {
+      "id": 3,
+      "title": "Salary",
+      "amount": 450,
+      "type": "income"
+    }
+  ])
 
   function handleTransactionInputValue(value,inputType){
     if(inputType === 'text') setTransactionTitle(value);
@@ -19,17 +39,21 @@ function App() {
   }
 
   return (
-    <div>
+    <div className=''>
       <h2 className='text-2xl mb-1'>Lucid Expense Tracker</h2>
       <div className='mb-10 border-t-2 w-24 mx-auto border-violet-400 border-opacity-80'></div>
-      <Portfolio totalExpense={-300} totalIncome={500} currentBalance={2000} />
-      <AddTransaction 
-        transactionTitle={transactionTitle} 
-        transactionAmount={transactionAmount} 
-        handleTransactionInputValue={handleTransactionInputValue}
-        handleAddTransaction = {handleAddTransaction}
-        />
-      <ExpenseHistory />
+      <div className='flex flex-row gap-14 p-5'>
+        <div className='w-80'>
+          <Portfolio totalExpense={-300} totalIncome={500} currentBalance={2000} />
+          <ExpenseHistory transactions = {transactions} />
+        </div>
+        <AddTransaction 
+            transactionTitle={transactionTitle} 
+            transactionAmount={transactionAmount} 
+            handleTransactionInputValue={handleTransactionInputValue}
+            handleAddTransaction = {handleAddTransaction}
+            />
+      </div>
     </div>
   )
 }
--- lucid_expense_tracker/src/components/ExpenseHistory.jsx
+++ lucid_expense_tracker/src/components/ExpenseHistory.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import HistoryCard from './HistoryCard'
 
-function ExpenseHistory() {
+function ExpenseHistory({ transactions }) {
   return (
     <div className='my-7'>
         <h2 className='text-left text-xl text-black'>History</h2>
         <hr className='mb-3 '></hr>
-        <HistoryCard transactionTitle='Shopping' transactionAmount={-200} />
-        <HistoryCard transactionTitle='Lending Return' transactionAmount={400} />
+        <div className=' max-h-96 overflow-scroll'>
+            {
+                transactions.map( transaction => (
+                    <HistoryCard key={transaction.id} transactionTitle={transaction.title} transactionAmount={transaction.amount} />
+                ))
+            }
+        </div>
     </div>
   )
 }
