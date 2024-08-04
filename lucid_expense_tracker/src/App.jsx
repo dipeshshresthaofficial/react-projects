54f2ade4be152a4904c231b8c9a4cd207f30cbeb
dipesh <dipeshshrestha0314@gmail.com>
2024-08-04 15:30:53 -0400

    updated portfolio with dynamic data

--- lucid_expense_tracker/src/App.jsx
+++ lucid_expense_tracker/src/App.jsx
@@ -6,11 +6,17 @@ import AddTransaction from './components/AddTransaction'
 
 function App() {
 
-  const [transactionTitle, setTransactionTitle] = useState("")
-  const [transactionAmount, setTransactionAmount] = useState("")
-  const [transactions, setTransactions] = useState([])
+  const [income,setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [balance, setBalance] = useState(0);
 
+  const [transactionTitle, setTransactionTitle] = useState("");
+  const [transactionAmount, setTransactionAmount] = useState("");
+  const [transactions, setTransactions] = useState([]);
+
+  // Fetching data from "./../data.json" which is simulated to be present in server for asynchronous call
   useEffect(()=>{
+    console.log("running useEffect");
     async function fetchData(){
       try{
         let resp = await fetch("./../data.json");
@@ -18,6 +24,22 @@ function App() {
           console.log("Unable to fetch data");
         }
         let data = await resp.json();
+
+        // Update Portfolio
+        let totalExpense = 0;
+        let totalIncome = 0;
+        let totalBalance = 0;
+
+        data.data.forEach( transaction => {
+          // updatePortfolio(transaction);
+          if(transaction.type === "expense")  totalExpense += transaction.amount;
+          else totalIncome += transaction.amount;
+        })
+        totalBalance = totalIncome + totalExpense;
+        setIncome(totalIncome);
+        setExpense(totalExpense);
+        setBalance(totalBalance);
+
         setTransactions([...data.data]); 
   
       } catch(err){
@@ -27,11 +49,24 @@ function App() {
     fetchData();
   },[])
 
+  // called for updating the profile section
+  function updatePortfolio(transaction){
+    if(transaction.type == "income"){
+      setIncome( prevIncome => transaction.amount + prevIncome);
+      setBalance(prevBalance => prevBalance + transaction.amount);
+    } else{
+      setExpense(prevExpense => transaction.amount + prevExpense);
+      setBalance(prevBalance => prevBalance + transaction.amount);
+    }
+  }
+
+  // called when any changes occur in input box in LabelAndInput component
   function handleTransactionInputValue(value,inputType){
     if(inputType === 'text') setTransactionTitle(value);
     else if(inputType === 'number') setTransactionAmount(value);
   }
 
+  // called when "add" button is clicked in AddTransaction component
   function handleAddTransaction(){
     if (transactionAmount == "" && transactionTitle == "")
       alert("Please make sure to add Transaction Title and Amount.")
@@ -51,6 +86,10 @@ function App() {
         "amount": Number(transactionAmount),
         "type": type
       }
+
+      // Update portfolio
+      updatePortfolio(newTransaction);
+
       // reset title and amount state
       setTransactionAmount("");
       setTransactionTitle("");
@@ -64,7 +103,7 @@ function App() {
       <div className='mb-10 border-t-2 w-24 mx-auto border-violet-400 border-opacity-80'></div>
       <div className='flex flex-row gap-14 p-5'>
         <div className='w-80'>
-          <Portfolio totalExpense={-300} totalIncome={500} currentBalance={2000} />
+          <Portfolio totalExpense={expense} totalIncome={income} currentBalance={balance} />
           <ExpenseHistory transactions = {transactions} />
         </div>
         <AddTransaction 
