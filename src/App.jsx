2c332d944248387a135747ceac00fb7719c0f720
dipesh <dipeshshrestha0314@gmail.com>
2024-08-14 17:15:13 -0400

    configured Routes and added skeleton of required components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import './App.css';
+import Feed from './components/Feed';
+import VideoDetail from './components/VideoDetail';
+import ChannelDetail from './components/ChannelDetail';
+import SearchFeed from './components/SearchFeed';
+import NavBar from './components/NavBar';
 
-function App() {
-
-  return (
-    <>
-      <div>Hello, World!</div>
-    </>
+const App = () => (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Feed />} />
+          <Route path='/video/:id' element = {<VideoDetail />} />
+          <Route path='/channel/:id' element = {<ChannelDetail />} />
+          <Route path='/search/searchTerm' element = {<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   )
-}
 
 export default App
