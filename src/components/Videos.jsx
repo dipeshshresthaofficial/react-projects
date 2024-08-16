37be9729c3e70a1841b68a6dd591942c30cc9206
dipesh <dipeshshrestha0314@gmail.com>
2024-08-16 01:25:13 -0400

    created Video Feed with VideoCard and ChannelCard

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,34 @@
+import { Box, Stack } from '@mui/material'
 import React from 'react'
+import { VideoCard, ChannelCard } from './index';
 
 const Videos = ({videos}) => {
   console.log(videos)
   return (
-    <div>Videos</div>
+    <Stack
+      direction='row'
+      flexWrap='wrap'
+      gap={2}
+      sx={{
+        height: {sx: 'auto', md: '95%'},
+        overflowY:'auto'
+      }}
+
+      >
+      {
+        videos.map((item,idx) => {
+          // there are other items like playlist info, we will discard them
+          if(!(item?.id?.channelId || item?.id?.videoId)) return;
+          // if the item is either channel or video then only we will return the Box ui
+          return (
+            <Box key={idx}>
+              { item.id.channelId && <ChannelCard channel = {item} />}
+              { item.id.videoId && <VideoCard video = {item} />}
+            </Box>
+          )
+        })
+      }
+    </Stack>
   )
 }
 
